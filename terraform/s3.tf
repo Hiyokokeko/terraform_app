00553f313c0f9d8aa8b376dc8df4b35c76cd7feb
Hiyokokeko <sukepon0914@gmail.com>
2021-02-09 13:34:50 +0900

    パブリックバケット
    アクセス権はacl、デフォはprivateなのでpablic-readにしてインターネット読み込み可能にする
    cors_ruleで許可するオリジンやメソッドを定義する

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,6 @@
+# プライベートバケット
 resource "aws_s3_bucket" "private" {
-  bucket = "private-pragmatic-terraform"
+  bucket = "private-pragmatic-terraform" # バケット名は一意制約よ
 
   versioning {
     enabled = true
@@ -14,10 +15,24 @@ resource "aws_s3_bucket" "private" {
   }
 }
 
+# ブロックパブリックアクセス
 resource "aws_s3_bucket_public_access_block" "private" {
   bucket                  = aws_s3_bucket.private.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+#パブリックバケット
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
 }
