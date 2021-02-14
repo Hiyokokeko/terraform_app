cfc3c03d5b237275051216983e6f88de62f32d2b
Hiyokokeko <sukepon0914@gmail.com>
2021-02-14 22:38:52 +0900

    プライベートのルートテーブルを作成
    VPCとの関連付け

--- terraform/vpc_route_table.tf
+++ terraform/vpc_route_table.tf
@@ -18,3 +18,9 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+# プライベートのルートテーブルにプライベートサブネット割り当て
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
