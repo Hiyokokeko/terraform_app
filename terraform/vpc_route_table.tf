28399716e6c8baddc36b3f93aac7eecb0c8aaadb
Hiyokokeko <sukepon0914@gmail.com>
2021-02-15 13:36:28 +0900

    NATゲートウェイとプライベートテーブルのルーティングを作成
    プライベートネットワークからインターネットへ通信するため

--- terraform/vpc_route_table.tf
+++ terraform/vpc_route_table.tf
@@ -13,6 +13,15 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
+# natゲートウェイとプライベートのルートテーブルを接続
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# 以下アソシエーション #
+
 # パブリックのルートテーブルにパブリックサブネットを割り当て
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
