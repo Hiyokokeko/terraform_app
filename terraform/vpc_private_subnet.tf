cfb4c5f0083e7c7e7e59079364a2c5883f01ed45
Hiyokokeko <sukepon0914@gmail.com>
2021-02-15 14:05:07 +0900

    NATゲートウェイのマルチAZ化
    プライベートサブネットについているNATゲートウェイをマルチAZ化

--- terraform/vpc_private_subnet.tf
+++ terraform/vpc_private_subnet.tf
@@ -1,6 +1,14 @@
-resource "aws_subnet" "private" {
+# プライベートサブネットのマルチAZ化
+resource "aws_subnet" "private_0" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.64.0/24"
+  cidr_block              = "10.0.65.0/24"
   availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = false
 }
