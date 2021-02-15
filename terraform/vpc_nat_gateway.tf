e5e522e69277a4b68115bb5031053261a514fb25
Hiyokokeko <sukepon0914@gmail.com>
2021-02-15 14:17:17 +0900

    NATゲートウェイのマルチAZ化
    パブリックサブネットについているNATゲートウェイをマルチAZ化

--- terraform/vpc_nat_gateway.tf
+++ terraform/vpc_nat_gateway.tf
@@ -1,12 +1,33 @@
-# NATゲートウェイの定義
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.nat_gateway.id
-  subnet_id     = aws_subnet.public.id
+# public_0のNATゲートウェイを作成
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
   depends_on    = [aws_internet_gateway.example]
+  tags = {
+    Name = "public_0_subnet_nat_gateway_0"
+  }
 }
-
-# EIPの定義
-resource "aws_eip" "nat_gateway" {
+resource "aws_eip" "nat_gateway_0" {
   vpc        = true
   depends_on = [aws_internet_gateway.example]
+  tags = {
+    Name = "eip0"
+  }
+}
+
+# public_1のNATゲートウェイを作成
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.example]
+  tags = {
+    Name = "public_1_subnet_nat_gateway_1"
+  }
+}
+resource "aws_eip" "nat_gateway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+  tags = {
+    Name = "eip1"
+  }
 }
