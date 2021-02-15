58469f093991cbd8f3ca632e60a9d95ab0b15c3a
Hiyokokeko <sukepon0914@gmail.com>
2021-02-15 13:45:00 +0900

    パブリックサブネットのマルチAZ化
    パブリックサブネットの複製
    AZ=アベイラビリティゾーン

--- terraform/vpc_public_subnet.tf
+++ terraform/vpc_public_subnet.tf
@@ -1,6 +1,20 @@
-resource "aws_subnet" "public" {
+# パブリックサブネットのマルチAZ化
+resource "aws_subnet" "public_0" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
+  tags = {
+    Name = "public_0_subnet"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "public_1_subnet"
+  }
 }
