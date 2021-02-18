8c2361873f9cfca3fc6b15e84649199527008a66
Hiyokokeko <sukepon0914@gmail.com>
2021-02-18 13:35:16 +0900

    DNSレコードを作成
    ・type
    AレコードやCNAMEレコードを指定できます
    Aを指定すると、AWSの独自拡張であるALIASレコードになります
    
    ・CNAMEレコード
    一般的に使用されるDNSレコードです
    ドメイン名→CNAMEレコードのドメイン名→IPアドレスという流れで名前解決を行います
    
    ・ALIASレコード
    AWSでのみ使用可能なDNSレコードです
    ALIASレコードはAWSの各種サービスと統合されており、
    AWSやS3バケット、CloudFrontも指定できます
    ドメイン名→IPアドレスという流れで名前解決が行われ、パフォーマンスが向上します

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,3 +7,20 @@ data "aws_route53_zone" "example" {
 resource "aws_route53_zone" "test_example" {
   name = "test.example.com" # ここはあとで自分のドメインに
 }
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = data.aws_route53_zone.example.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+# ドメイン名を出力
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
