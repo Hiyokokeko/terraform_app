3af8a8a45093f195faffee312887beb883b969c6
Hiyokokeko <sukepon0914@gmail.com>
2021-02-18 13:57:56 +0900

    検証用DNSレコードの作成

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -8,3 +8,12 @@ resource "aws_acm_certificate" "example" {
     create_before_destroy = true
   }
 }
+
+# SSL証明書用の検証用レコード定義
+resource "aws_route53_record" "example_certificate" {
+  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = data.aws_route53_zone.example.id
+  ttl     = 60
+}
