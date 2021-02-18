d11d1b3f8f3caf0b09597ad6a7e130dd82a5d7c5
Hiyokokeko <sukepon0914@gmail.com>
2021-02-18 13:46:15 +0900

    SSL証明書を定義
    ・ACMとは
    AWS Certificate Managerの略です
    Certificateは証明という意味です
    煩雑なSSL証明書の管理を担ってくれるマネージドサービスで、
    ドメイン検証をサポートしてくれます
    SSL証明書の自動更新も行ってくれます
    
    ・subject_alternative_names
    ドメイン名を追加する場合に設定します
    追加しない場合は空にします
    
    ・validation_method
    ドメインの所有権の検証方法を設定できます
    DNS検証かEメール検証を選択できます
    SSL証明書を自動更新したい場合はDNS検証を選択します
    
    ・lifecycle
    terraformの全てのリソースは、通常は
    リソースを削除してからリソースを作成するという挙動ですが、
    create_before_destroyをtrueにすることで、
    リソースを作成してからリソースを削除するという挙動に変更できます
    新しいSSL証明書を作ってから、古いSSL証明書と差し替えるという挙動に変更し、
    SSL証明書の再作成時のサービス影響を最小化します

---
+++ terraform/acm.tf
@@ -0,0 +1,10 @@
+# SSL証明書作成
+resource "aws_acm_certificate" "example" {
+  domain_name               = aws_route53_record.example.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
