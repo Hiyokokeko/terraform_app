ecd9ad0aba0d914d30f0b588540487a271294a32
Hiyokokeko <sukepon0914@gmail.com>
2021-02-19 11:17:08 +0900

    自分のドメインに変えました
    AWSのRoute53にてドメイン登録をしたので、変更しました

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,11 +1,11 @@
 # ドメイン名設定
 # ホストゾーンの参照
 data "aws_route53_zone" "example" {
-  name = "example.com" # ここはあとで自分のドメインに
+  name = "realshinkitv.com"
 }
-# ホストゾーンの作成
-resource "aws_route53_zone" "test_example" {
-  name = "test.example.com" # ここはあとで自分のドメインに
+# # ホストゾーンの作成
+# resource "aws_route53_zone" "test_example" {
+#   name = "test.example.com" # ここはあとで自分のドメインに
 }
 
 resource "aws_route53_record" "example" {
