74ba8884784f68f3393843131350168acb38f151
Hiyokokeko <sukepon0914@gmail.com>
2021-02-19 15:56:22 +0900

    ファイル名の変更しディレクトリを移動
    sourceの変更もしました

--- terraform/security_group_module.tf
+++ terraform/security_group_module.tf
@@ -1,7 +1,7 @@
 # リスト7.21 セキュリティグループモジュールの利用
 # VPCにセキュリティグループを設定
 module "example_sg" {
-  source      = "./security_group"
+  source      = "./modules/security_group"
   name        = "module-sg"
   vpc_id      = aws_vpc.example.id
   port        = 80
@@ -11,7 +11,7 @@ module "example_sg" {
 # ALBにセキュリティグループを設定
 # http用
 module "http_sg" {
-  source      = "./security_group"
+  source      = "./modules/security_group"
   name        = "http-sg"
   vpc_id      = aws_vpc.example.id
   port        = 80
@@ -19,7 +19,7 @@ module "http_sg" {
 }
 # https用
 module "https_sg" {
-  source      = "./security_group"
+  source      = "./modules/security_group"
   name        = "https-sg"
   vpc_id      = aws_vpc.example.id
   port        = 443
@@ -27,7 +27,7 @@ module "https_sg" {
 }
 # httpリダイレクト用
 module "http_redirect_sg" {
-  source      = "./security_group"
+  source      = "./modules/security_group"
   name        = "http-redirect-sg"
   vpc_id      = aws_vpc.example.id
   port        = 8080
