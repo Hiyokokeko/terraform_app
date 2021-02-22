16cea5a1ee47a0210913f3dc2b8a6bdcc2d39aa6
Hiyokokeko <sukepon0914@gmail.com>
2021-02-22 12:18:46 +0900

    ECSタスク定義にECSタスク実行IAMロールを追加
    DockerコンテナがCloudWatch Logsにログを投げられるようにexecution_role_arnを追加しました

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -11,6 +11,7 @@ resource "aws_ecs_task_definition" "example" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./tasks/container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
 
 # ECSサービス作成
