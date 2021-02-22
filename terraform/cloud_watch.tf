b203f8def35583a246f407697c8103e108233ca7
Hiyokokeko <sukepon0914@gmail.com>
2021-02-22 15:29:12 +0900

    バッチ用CloudWatch Logsの定義を作成
    ・name
    パス
    ・retention_in_days
    保存日数を指定

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -3,3 +3,9 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name              = "/ecs/example"
   retention_in_days = 180
 }
+
+# ECS Scheduled Tasks用のログ作成
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
