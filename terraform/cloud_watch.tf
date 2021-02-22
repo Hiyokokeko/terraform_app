aeb03d7a1e8a16503a2e6765414e5cb49cea8e38
Hiyokokeko <sukepon0914@gmail.com>
2021-02-22 16:09:48 +0900

    CloudWatchイベントルールの作成
    ・description
    説明です
    日本語も使用できます
    
    ・schedule_expression
    cron式やrate式を指定できます
    2分ごとにログを取得するようにします

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -9,3 +9,10 @@ resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
   name              = "/ecs-scheduled-tasks/example"
   retention_in_days = 180
 }
+
+# ECS Scheduled Tasks用のバッチ処理
+resource "aws_cloudwatch_event_rule" "example_batch" {
+  name                = "example-batch"
+  description         = "とても重要なバッチ処理です"
+  schedule_expression = "cron(*/2 * * * ? *)"
+}
