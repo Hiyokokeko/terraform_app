5315dd26432b1340b44d7cf22d817f454765c06d
Hiyokokeko <sukepon0914@gmail.com>
2021-02-22 12:52:28 +0900

    apply時にError: Invalid indexが出たので修正
    acm.tfの記述を修正

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -11,9 +11,9 @@ resource "aws_acm_certificate" "example" {
 
 # SSL証明書用の検証用レコード定義
 resource "aws_route53_record" "example_certificate" {
-  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  name    = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_type
+  records = [tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_value]
   zone_id = data.aws_route53_zone.example.id
   ttl     = 60
 }
