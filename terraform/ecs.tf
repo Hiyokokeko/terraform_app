1ac73dec84186d1015289de2023adbebe302cd57
Hiyokokeko <sukepon0914@gmail.com>
2021-02-22 15:42:44 +0900

    バッチ用タスク定義を作成
    ECSの時と同様なので解説省略

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -44,3 +44,14 @@ resource "aws_ecs_service" "example" {
     ignore_changes = [task_definition]
   }
 }
+
+# ECS Scheduled Tasks用タスク定義作成
+resource "aws_ecs_task_definition" "example_batch" {
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./tasks/container_definitions/batch_ecs_task_do.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
