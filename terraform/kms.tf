ba4a59878519ea531566b1a86cdf5c57ed43413f
Hiyokokeko <sukepon0914@gmail.com>
2021-02-23 23:43:02 +0900

    カスタマーマスターキーの定義
    ・description
    説明を記述します
    
    ・enable_key_rotation
    年に一度の自動ローテーション機能の有効・無効を設定します
    
    ・is_enabled
    カスタマーマスターキーの有効・無効を設定します
    
    ・deletion_window_in_days
    カスタマーマスターキーの完全削除までの日数を指定します
    デフォルトは30日です

---
+++ terraform/kms.tf
@@ -0,0 +1,7 @@
+# 暗号・復号化用鍵作成
+resource "aws_kms_key" "example" {
+  description             = "Example Customer Master Key"
+  enable_key_rotation     = true
+  is_enabled              = true
+  deletion_window_in_days = 30
+}
