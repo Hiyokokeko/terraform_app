6f5acb8625ce54c3c1e16a86e7f13909fa4a613b
Hiyokokeko <sukepon0914@gmail.com>
2021-02-26 13:36:36 +0900

    ElastiCacheサブネットグループを作成
    ・name
    名前を指定します
    ・subnet_ids
    サブネットを指定します
    今回の場合RDSの時と同様に、プライベートサブネットを指定します

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -8,3 +8,9 @@ resource "aws_elasticache_parameter_group" "example" {
     value = "no"
   }
 }
+
+# ElastiCache用サブネットグループ
+resource "aws_elasticache_subnet_group" "example" {
+  name       = "example"
+  subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
+}
