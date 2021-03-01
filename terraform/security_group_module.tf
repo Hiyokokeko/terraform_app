aa96e3b448cbdb04b7dfc39f79c7cb3ab0d2fe0b
Hiyokokeko <sukepon0914@gmail.com>
2021-03-01 21:52:09 +0900

    スローapply問題
    RDSやElastiCacheのapplyには時間がかかるので、一旦コメントアウトしました

--- terraform/security_group_module.tf
+++ terraform/security_group_module.tf
@@ -44,19 +44,19 @@ module "nginx_sg" {
 }
 
 # RDS(MySQL)用
-module "mysql_sg" {
-  source      = "./modules/security_group"
-  name        = "mysql-sg"
-  vpc_id      = aws_vpc.example.id
-  port        = 3306
-  cidr_blocks = [aws_vpc.example.cidr_block]
-}
+# module "mysql_sg" {
+#   source      = "./modules/security_group"
+#   name        = "mysql-sg"
+#   vpc_id      = aws_vpc.example.id
+#   port        = 3306
+#   cidr_blocks = [aws_vpc.example.cidr_block]
+# }
 
-# ElastiCache用
-module "redis_sg" {
-  source      = "./modules/security_group"
-  name        = "redis-sg"
-  vpc_id      = aws_vpc.example.id
-  port        = 6379
-  cidr_blocks = [aws_vpc.example.cidr_block]
-}
+# # ElastiCache用
+# module "redis_sg" {
+#   source      = "./modules/security_group"
+#   name        = "redis-sg"
+#   vpc_id      = aws_vpc.example.id
+#   port        = 6379
+#   cidr_blocks = [aws_vpc.example.cidr_block]
+# }
