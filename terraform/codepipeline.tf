79492f00986dfbecb4a32b9323026f9a48520dc6
Hiyokokeko <sukepon0914@gmail.com>
2021-03-02 13:01:34 +0900

    CodeBuildのポリシードキュメントを作成
    次の権限を付与します。
    ・ビルド出力アーティファクトを保存するためのS3操作権限
    ・ビルドログを出力するためのCloudWatchLogs操作権限
    ・DockerイメージをプッシュするためのECR操作権限

---
+++ terraform/codepipeline.tf
@@ -0,0 +1,31 @@
+# CodeBuildで下記権限付与
+# ビルド出力アーティファクトを保存するためのS3操作権限
+# ビルドログを出力するためのCloudWatch Logs操作権限
+# DockerイメージをプッシュするためのECR操作権限
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+  }
+}
