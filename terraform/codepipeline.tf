1bc419a78c0ccc691082efd7fc608527e7f62b90
Hiyokokeko <sukepon0914@gmail.com>
2021-03-02 16:02:02 +0900

    CodePipeline用のIAMロールを定義

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -85,3 +85,10 @@ data "aws_iam_policy_document" "codepipeline" {
     ]
   }
 }
+
+module "codepipeline_role" {
+  source     = "./modules/iam_role"
+  name       = "codepipeline"
+  identifier = "codepipeline.amazonaws.com"
+  policy     = data.aws_iam_policy_document.codepipeline.json
+}
