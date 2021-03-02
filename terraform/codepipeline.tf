a13224ce0b02d592a2b07bd9e397a3870c2425c1
Hiyokokeko <sukepon0914@gmail.com>
2021-03-02 13:16:48 +0900

    Codebuildプロジェクトを作成
    ・name
    名前を指定します
    
    ・service_role
    IAMロールのARNを指定します
    
    ・source
    ・artifacts
    sourceにビルド対象のファイルを指定し、artifactsにビルド出力アーティファクトの格納先を指定します
    両方ともCODEPIPELINEとすることで、CodePipelineと連携することを宣言します
    
    ・environment
    imageに指定しているaws/codebuild/standard:2.0はAWSの管理しているUbuntuベースのイメージです
    ビルド時にdockerコマンドを使う場合は、privileged_modeをtrueにします

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -36,3 +36,24 @@ module "codebuild_role" {
   identifier = "codebuild.amazonaws.com"
   policy     = data.aws_iam_policy_document.codebuild.json
 }
+
+# CodeBuild作成
+resource "aws_codebuild_project" "example" {
+  name         = "example"
+  service_role = module.codebuild_role.iam_role_arn
+
+  source {
+    type = "CODEPIPELINE"
+  }
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type            = "LINUX_CONTAINER"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:2.0"
+    privileged_mode = true
+  }
+}
