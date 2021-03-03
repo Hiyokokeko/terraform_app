abba6536388503527f2c9cbc944677008145dfe2
Hiyokokeko <sukepon0914@gmail.com>
2021-03-03 13:35:54 +0900

    CodePipeline Webhookの定義
    GithubからWebhookを受け取るために作成します
    
    ・target_pipeline
    Webhookを受け取ったら起動するパイプラインをtarget_pipelineで設定します
    
    ・target_action
    最初に実行するアクションを指定します
    
    ・authentication
    GitHubのWebhookはHMACによるメッセージ認証をサポートしているため、GITHUB_HMACを指定します
    
    ・authentication_comfiguration
    secret_tokenに、ランダムな20バイト以上の文字列を秘密鍵として指定します
    この秘密鍵は、GitHub側のWebHookでも使用します

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -161,3 +161,20 @@ resource "aws_codepipeline" "example" {
     type     = "S3"
   }
 }
+
+# CodePipeline Webhook
+resource "aws_codepipeline_webhook" "example" {
+  name            = "example"
+  target_pipeline = aws_codepipeline.example.name
+  target_action   = "Source"
+  authentication  = "GITHUB_HMAC"
+
+  authentication_configuration {
+    secret_token = var.WEBHOOK_TOKEN
+  }
+
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+}
