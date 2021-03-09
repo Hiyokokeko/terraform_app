820fc47d1a5f4b09bb10a6919cec3181d802f064
Hiyokokeko <sukepon0914@gmail.com>
2021-03-09 16:07:10 +0900

    CloudWatch Logsサブスクリプションフィルタの定義
    ・log_group_name
    関連付けるロググループ名の指定をします
    ここではバッチ用おCloudWatch Logsを指定します
    
    ・destination_arn
    ログの送信先です
    KinesisDataFirehose配信ストリームを指定しています
    
    ・filter_pattern
    Kinesis Data Firehoseに流すデータをフィルタリングできます
    ここでは、フィルタリングせずにすべて送る設定を記述しています
    
    ・role_arn
    先ほど作成した、CloudWatch Logs IAMロールを設定します

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -39,6 +39,15 @@ resource "aws_cloudwatch_event_target" "example_batch" {
 
 # オペレーションログを保存するCloudWatch Logsの定義
 resource "aws_cloudwatch_log_group" "operation" {
-  name = "/operation"
+  name              = "/operation"
   retention_in_days = 180
+}
+
+# CloudWatchLogsサブスクリプションフィルタを作成
+resource "aws_cloudwatch_log_subscription_filter" "example" {
+  name            = "example"
+  log_group_name  = aws_cloudwatch_log_group.for_ecs_scheduled_tasks.name
+  destination_arn = aws_kinesis_firehose_delivery_stream.example.arn
+  filter_pattern  = "[]"
+  role_arn        = module.cloudwatch_logs_role.iam_role_arn
 }
