e287b80b1956f2beed8243fc5283bdd8a60e7855
Hiyokokeko <sukepon0914@gmail.com>
2021-03-09 14:01:13 +0900

    Kinesis Data Firehoseの配信ストリームが使用するIAMポリシードキュメントの作成

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -105,3 +105,24 @@ resource "aws_iam_instance_profile" "ec2_for_ssm" {
   name = "ec2-for-ssm"
   role = module.ec2_for_ssm_role.iam_role_name
 }
+
+# Kinesis Data Firehose用の操作権限
+data "aws_iam_policy_document" "kinesis_data_firehose" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}",
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*",
+    ]
+  }
+}
