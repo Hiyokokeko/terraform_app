1754681aea99c21861dd0f3231782b667e2cef9b
Hiyokokeko <sukepon0914@gmail.com>
2021-03-16 10:57:20 +0900

    自分のリージョンを出力するようにしました

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,3 +4,9 @@ data "aws_caller_identity" "current" {}
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+# リージョンを出力
+data "aws_region" "current" {}
+output "region_name" {
+  value = data.aws_region.current.name
+}
