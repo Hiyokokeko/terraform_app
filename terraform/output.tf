2ce3d1f84fe9a532631f5f2cb860b82b81f9f46a
Hiyokokeko <sukepon0914@gmail.com>
2021-03-16 10:59:35 +0900

    ELB用のIDを出力するようにしました

--- terraform/output.tf
+++ terraform/output.tf
@@ -18,3 +18,9 @@ data "aws_availability_zones" "available" {
 output "availability_zones" {
   value = data.aws_availability_zones.available.names
 }
+
+# ELB用のIDを出力
+data "aws_elb_service_account" "current" {}
+output "alb_service_account_id" {
+  value = data.aws_elb_service_account.current.id
+}
